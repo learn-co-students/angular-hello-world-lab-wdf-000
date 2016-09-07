dad64dfaac9d62169a6042dec73119f9328db14d
blessedsoy <unmigong@gmail.com>
2016-09-07 11:13:49 -0400

    Done.

--- js/app/controllers/MainController.js
+++ js/app/controllers/MainController.js
@@ -3,3 +3,7 @@ function MainController($scope){
   $scope.email: 'bill@gates.com',
   $scope.phone: '01234567890';
 }
+
+angular
+  .module('app')
+  .controller('MainController', MainController);
--- js/app/controllers/app.js
+++ js/app/controllers/app.js
@@ -1,1 +1,2 @@
-app.js
+angular
+  .module('app', []);
